b6d8bdc658054f8174d1c7d9bcad2c12ab974590
JustoSinak <tcheumanisinakjusto@gmail.com>
2024-11-28 08:39:24 +0100

    Updated App.tsx with login and signup functionality

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Login and Signup app 
 
-export default function App() {
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './components/Home';
+import LoginScreen from './components/LoginScreen';
+import SignupScreen from './components/SignupScreen';
+
+const Stack =createNativeStackNavigator();
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name={"Home"} component={Home} />
+      <Stack.Screen name={"Login"} component={LoginScreen} />
+      <Stack.Screen name={"Signup"} component={SignupScreen} />
+    </Stack.Navigator>
+   </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    top: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inText:{
+    color: '#45fa76',
+    fontSize: 40
+  }
 });
